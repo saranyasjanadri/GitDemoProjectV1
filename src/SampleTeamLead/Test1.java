c36f40739fc5989898c3c3caf2883f2f61b0152c
shriya <saranyainfor@gmail.com>
2022-12-11 22:16:15 +0530

    first commit

---
+++ src/SampleTeamLead/Test1.java
@@ -0,0 +1,19 @@
+package SampleTeamLead;
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{
+		String hungry ="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("Eat something");
+		}
+		else
+		{
+			System.out.println("Do work");
+		}
+		
+	}
+
+}
